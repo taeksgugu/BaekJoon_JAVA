b27d62c3df50a37dc71d2951808ce1ed483cf93a
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2024-03-08 13:01:05 +0900

    [Bronze III] Title: 오븐 시계, Time: 76 ms, Memory: 11576 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws java.io.IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));   //할당된 버퍼에 값 넣어주기
+		StringTokenizer st = new StringTokenizer(br.readLine());; //StringTokenizer인자값에 입력 문자열 넣음
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int c = Integer.parseInt(st.nextToken());
+		int hour = (a + (b+c)/60)%24;
+		int minute = (b+c)%60; 
+		bw.write(hour + " " + minute);
+		bw.close();
+	}
+}
