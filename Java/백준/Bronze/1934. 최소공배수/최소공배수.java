cf125627d92692b1e45c54c74db7536e0766de52
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2024-03-08 14:28:34 +0900

    [Bronze I] Title: 최소공배수, Time: 336 ms, Memory: 15096 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws java.io.IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));   //할당된 버퍼에 값 넣어주기
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int result = a*b;
+			for (int j = 1; j <= a; j++) {
+				if (a%j==0 && b%j==0) {
+					result = (a/j) * b;
+				}
+			}	
+			bw.write(result+"\n");
+		}
+		bw.close();
+	}
+}
