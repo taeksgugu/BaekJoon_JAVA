6f19b61e1df43406094287e8dfc9f8729fd77089
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2024-03-08 10:37:49 +0900

    [Bronze V] Title: A+B - 8, Time: 80 ms, Memory: 11700 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws java.io.IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));   //할당된 버퍼에 값 넣어주기
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			bw.write("Case #" + (i+1) + ": " + a + " + " + b +
+					" = " + (a+b) + "\n");
+		}
+		
+		bw.close();
+	}
+}
