e305b3d98d9ba18661fc8f413d5e8f3f3c4a9565
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2024-03-12 10:01:58 +0900

    [level 0] Title: 이어 붙인 수, Time: 1.62 ms, Memory: 65.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String even = "", odd="";
+        for (int num: num_list) {
+            if (num%2==0) {
+                even += String.valueOf(num);
+            } else {
+                odd += String.valueOf(num);
+            }
+        }
+        return Integer.parseInt(even) + Integer.parseInt(odd);
+    }
+}
