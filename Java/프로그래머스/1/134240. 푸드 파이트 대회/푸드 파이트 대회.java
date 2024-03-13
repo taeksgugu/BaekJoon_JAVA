bc61c3333b30ac66be0d91a815f8eff5f8718ccd
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2024-03-13 10:53:20 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 14.98 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        for (int i=1;i<food.length;i++) {
+			for (int j = 0; j < food[i]/2; j++) {
+				answer += i+"";
+			}
+		}
+		StringBuffer sb = new StringBuffer(answer);
+		return answer + 0 + sb.reverse().toString();
+    }
+}
