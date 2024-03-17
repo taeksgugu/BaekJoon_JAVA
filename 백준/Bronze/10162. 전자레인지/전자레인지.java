11e735dc516fb297a22004b28920d8c8d8ec100d
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2024-03-17 16:22:36 +0900

    [Bronze III] Title: 전자레인지, Time: 76 ms, Memory: 11628 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -0,0 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main{
+
+	public static void main(String[] args) throws java.io.IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));   //할당된 버퍼에 값 넣어주기
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int a = 0, b = 0, c = 0;
+		
+		a += n/300;
+		n%=300;
+		b += n/60;
+		n%=60;
+		c += n/10;
+		n%=10;
+		
+		if (n==0) bw.write(a + " " + b + " " + c);
+		else bw.write(-1+"");
+		bw.close();
+	}
+}
