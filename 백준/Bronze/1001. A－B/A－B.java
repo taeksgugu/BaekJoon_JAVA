36d8fd05050cc21aaae7a2a5ad119f6a555f9738
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2024-03-22 15:31:21 +0900

    [Bronze V] Title: A-B, Time: 116 ms, Memory: 12840 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int A;
+		int B;
+		int C;
+		Scanner sc = new Scanner(System.in);
+		A = sc.nextInt();
+		B = sc.nextInt();
+        
+		C = A-B;
+        System.out.println(C);
+	}
+
+}
